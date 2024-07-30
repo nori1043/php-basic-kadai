5f42d96c980ced6d5803420b203bf2d9dde8cbc4
nori1043 <seki_1043@icloud.com>
2024-07-30 21:13:32 +0900

    フォルダをアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>課題007</title>
+</head>
+<body>
+  <p>
+    <?php
+     $date = ['name' => 'onion' , 'price' => '200' , 'weight' => '160'];
+     
+     print_r($date);
+
+     ?>
+  </p>
+</body>
+</html>
